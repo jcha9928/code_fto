017f081c739fad2a584d91ec37f6a9441faf2bb7
Jiook Cha <cha.jiook@gmail.com>
2018-03-11 23:00:21 -0400

    Update trac_1.sh

--- trac_1.sh
+++ trac_1.sh
@@ -15,9 +15,8 @@ rm -rf $CMD_batch
 #######################################################################################################
 cat<<EOC >$CMD_batch
 #!/bin/bash
-#COBALT -t 01:00:00
+#COBALT -t 12:00:00
 #COBALT -n $N
-#COBALT -q debug-cache-quad
 #COBALT --attrs mcdram=cache:numa=quad:ssds=required:ssd_size=40 
 #COBALT -A AD_Brain_Imaging
 #COBALT -M jiook.cha@nyspi.columbia.edu
